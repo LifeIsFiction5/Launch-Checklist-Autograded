529be4a961325ac3706e5e7e8579686aab52d01d
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-08-12 23:23:43 -0500

    Put in some work on the functions portion of GA5

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-// Write your JavaScript code here!
+ const { formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+const launchForm = document.querySelector("form");
+const list = document.getElementById("faultyItems");
+
+
+
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
+    launchForm.addEventListener("submit", function (event){ 
+        event.preventDefault();
+
+        const pilotName = document.querySelector("input[name=pilotName]").value;
+        const copilotName = document.querySelector("input[name=copilotName]").value;  
+        const fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        const cargoMass = document.querySelector("input[name=cargoMass]").value;
+        
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
     })
     
+
+
  });
+
+ //There will be a function to addEventListener to the submitForm().  This is where I will have pilot name, copilot name, etc.
+
+ //call formSumbission in script.js with the values from the form
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,13 +17,47 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     */
  }
  
- function validateInput(testInput) {
+ function validateInput(input) {
+    const numInput = Number(input);
     
+    if (!input) {
+        return "Empty";
+    } else if (!isNaN(numInput)) {
+        return "Is a Number";
+    } else if (isNaN(numInput)) {
+        return "Not a Number";
+    }
  }
+ 
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     
- }
+    const pilotStatus = document.getElementById("pilotStatus");
+    const copilotStatus = document.getElementById("copilotStatus");
+    const fuelLevelStatus = document.getElementById("fuelStatus");
+    const cargoLevelStatus = document.getElementById("cargoStatus");
+
+    if (validateInput(pilot) === "Empty" || 
+    validateInput(copilot) === "Empty" || 
+    validateInput(fuelLevel) === "Empty" || 
+    validateInput(cargoLevel) === "Empty") 
+    {
+        alert("All fields are required!");
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number")  {
+        alert("That pilot isn't a number!");
+    } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number" ) {
+        alert("Fuel Level and Cargo Mass must be a Number!")
+    } else {
+        list.style.visibility = "visible";
+        pilot.innerHTML = `Pilot ${pilot} is ready for Launch!`;
+        copilot.innerHTML = `Copilot ${copilot} is ready for Launch!`;
+
+        //if was here
+    }
+    }
+
+    //fuelLevelStatus.innerHTML = value
+    //Call function, take statuses and change the HTML on the page (probably using .innerHTML) depending on what fuelLevelStatus and cargoLevelStatus is.
  
  async function myFetch() {
      let planetsReturned;
